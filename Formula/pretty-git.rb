06db43a9349d0442b98a632853ab5117df8286c0
Mikhail Matskevich <182345589+MikoMikocchi@users.noreply.github.com>
2025-08-13 03:40:20 +0300

    fix: update formula

--- Formula/pretty-git.rb
+++ Formula/pretty-git.rb
@@ -1,19 +1,16 @@
 class PrettyGit < Formula
   desc "Git repository analytics and reporting CLI"
   homepage "https://github.com/MikoMikocchi/pretty-git"
-  url "https://github.com/MikoMikocchi/pretty-git/archive/refs/tags/v0.1.0.tar.gz"
+  url "https://rubygems.org/downloads/pretty-git-0.1.0.gem"
   sha256 "b4f66c251f6384c686f0ee074376a1a1bf74a219fab33d82bc7fbee6a4d7c707"
   license "MIT"
 
   depends_on "ruby"
 
   def install
-    system "gem", "build", "pretty-git.gemspec"
-    gem_file = Dir["pretty-git-*.gem"].first
-
     ENV["GEM_HOME"] = buildpath/"gemhome"
     ENV["GEM_PATH"] = ""
-    system "gem", "install", gem_file, "--no-document",
+    system "gem", "install", cached_download, "--no-document",
            "--install-dir", ENV["GEM_HOME"], "--ignore-dependencies"
 
     rm_rf libexec
@@ -24,6 +21,6 @@ class PrettyGit < Formula
   end
 
   test do
-    system "#{bin}/pretty-git", "--version"
+    system "#{bin}/pretty-git", "--help"
   end
 end
