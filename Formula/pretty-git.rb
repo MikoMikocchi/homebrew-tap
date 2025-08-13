1f4dc93ced2d3a859e21c749d30aba5aaa56fc0f
Mikhail Matskevich <182345589+MikoMikocchi@users.noreply.github.com>
2025-08-13 03:58:52 +0300

    fix: update formula

--- Formula/pretty-git.rb
+++ Formula/pretty-git.rb
@@ -4,21 +4,27 @@ class PrettyGit < Formula
   url "https://rubygems.org/downloads/pretty-git-0.1.0.gem"
   sha256 "5caebad53d746b711a07b9a190d0a5cbc2910c60447a759effae5eb9a4cd6def"
   license "MIT"
-  revision 1
+  revision 2
 
   depends_on "ruby"
 
   def install
-    ENV["GEM_HOME"] = buildpath/"gemhome"
-    ENV["GEM_PATH"] = ""
-    system "gem", "install", cached_download, "--no-document",
-           "--install-dir", ENV["GEM_HOME"], "--ignore-dependencies"
-
     rm_rf libexec
-    libexec.install Dir[buildpath/"gemhome/*"]
 
-    bin.install_symlink Dir[libexec/"bin/*"]
-    bin.env_script_all_files libexec/"bin", GEM_HOME: libexec
+    vendor = libexec/"vendor"
+    vendor.mkpath
+    ENV["GEM_HOME"] = vendor
+    ENV["GEM_PATH"] = ""
+
+    system "gem", "install", cached_download, "--no-document",
+           "--install-dir", vendor, "--ignore-dependencies"
+
+    (bin/"pretty-git").write <<~SH
+      #!/bin/bash
+      export GEM_HOME="#{vendor}"
+      exec "#{vendor}/bin/pretty-git" "$@"
+    SH
+    (bin/"pretty-git").chmod 0755
   end
 
   test do
